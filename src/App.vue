af5820f30b56b6571563f3939751fa4471b76d80
grdhog <grdhog@users.noreply.github.com>
2023-01-23 15:49:39 +1300

    created array of adjusted closing price.

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,31 @@ export default defineComponent({
     let aDate = new Date();
     aDate.setFullYear(aDate.getFullYear() - 1);
     this.period1 = aDate.getTime();
-    this.period2 = Date.now();
+    this.period2 = Date.now();    
   },
   data: {
     csv: '',
     period1: 0,
-    period2: 0  
+    period2: 0,
+    closing: []
   },
   methods: {
+    convertCSV: function(){
+      const THE_DATE = 0;
+      const ADJUSTED_CLOSE = 5;
+      const lines = this.csv.split('\n');
+      console.log('num lines', lines.length);
+      const headers = lines.shift();
+      console.log('headers', headers);
+      const array = [];
+      for (const line of lines){
+        let parts = line.split(',');
+        array.push({'date': parts[THE_DATE], 'price': parts[ADJUSTED_CLOSE]});
+      }
+      this.closing = array;
+    },
     getTicker: function () {
-      let ticker = this.$refs.ticker.value.trim();
-      console.log(ticker);
-
+      let ticker = this.$refs.ticker.value.trim();    
       let params = {};
       params.period1 = this.period1;
       params.period2 = this.period2;
@@ -31,6 +44,7 @@ export default defineComponent({
       var that = this
       oReq.addEventListener('load', function () {
         that.csv = this.responseText;
+        that.convertCSV();
       })
       oReq.addEventListener('error', function () {
         console.error('API not working!')
@@ -48,6 +62,7 @@ export default defineComponent({
     <input ref="ticker" />
     <button v-on:click="getTicker">Get stock</button>
     <p>{{ csv }}</p>
+    <p>{{ closing }}</p>
   </div>
 </template>
 
