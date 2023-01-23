2954ce041b545e58cbb9997f0241ac806dccfe2f
grdhog <grdhog@users.noreply.github.com>
2023-01-23 15:26:03 +1300

    Now have a date range

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,28 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'App',
   mounted: function () {
-    console.log('mounted')
+    console.log('mounted');
+    let aDate = new Date();
+    aDate.setFullYear(aDate.getFullYear() - 1);
+    this.period1 = aDate.getTime();
+    this.period2 = Date.now();
   },
   data: {
-    csv: ''
+    csv: '',
+    period1: 0,
+    period2: 0  
   },
   methods: {
     getTicker: function () {
       let ticker = this.$refs.ticker.value.trim();
       console.log(ticker);
-      var url = 'https://hello-get-n5tpywpmka-uc.a.run.app?ticker=' + ticker;
+
+      let params = {};
+      params.period1 = this.period1;
+      params.period2 = this.period2;
+      params.ticker = ticker;
+      const qs = '?' + new URLSearchParams(params).toString();
+      var url = 'https://hello-get-n5tpywpmka-uc.a.run.app' + qs;
       console.log('url', url)
       var oReq = new XMLHttpRequest()
       var that = this
@@ -32,6 +44,7 @@ export default defineComponent({
 
 <template>
   <div id="app">
+    <p>period1: {{ period1 }} period2: {{ period2 }}</p>
     <input ref="ticker" />
     <button v-on:click="getTicker">Get stock</button>
     <p>{{ csv }}</p>
